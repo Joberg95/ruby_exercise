79a05a39f3ee7d4a810bad4e550ad02d5be3fcf8
nevroje <joakim.berglind@gmail.com>
2018-12-13 14:10:30 +0100

    Ruby exercise

---
+++ my_group.rb
@@ -0,0 +1,9 @@
+my_group = [person_1, person_2, person_3]
+
+person_1 = {name: "Jocke", age: 23, gender: male}
+person_2 = {name: "Jessica", age: 25, gender: "female"}
+person_3 = {name: "Erik", age: 24, gender: "male"}
+
+my_group.each do |name|
+    puts "#{my_group[:name] Hello!
+end
