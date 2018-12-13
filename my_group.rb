a2f67274e173a307fd72297a82daf07b4820e9e1
nevroje <joakim.berglind@gmail.com>
2018-12-13 15:21:53 +0100

    updated ruby exercise

--- my_group.rb
+++ my_group.rb
@@ -5,5 +5,5 @@ person_2 = {name: "Jessica", age: 25, gender: "female"}
 person_3 = {name: "Erik", age: 24, gender: "male"}
 
 my_group.each do |name|
-    puts "#{my_group[:name] Hello!
+    puts "#{my_group[:name]} Hello"
 end
